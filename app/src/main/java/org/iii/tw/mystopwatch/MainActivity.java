047f9adf9c7f6cb59489fa243a09f94781debeb4
chuan1983 <chuan1983126@gmail.com>
2016-09-13 11:24:27 +0800

    v1

--- app/src/main/java/org/iii/tw/mystopwatch/MainActivity.java
+++ app/src/main/java/org/iii/tw/mystopwatch/MainActivity.java
@@ -2,12 +2,25 @@ package org.iii.tw.mystopwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnLeft,btnRight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLeft = (Button)findViewById(R.id.btnLeft);
+        btnRight = (Button)findViewById(R.id.btnRight);
+
+    }
+
+    public void doLeft(View v){
+
+    }
+    public void doRight(View v){
+
     }
 }
